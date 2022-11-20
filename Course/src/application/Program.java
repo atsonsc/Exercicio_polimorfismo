154734dfed68b88481fbf7c46d6b73e1d5e4532c
Atson Conceição <atsonsc@gmail.com>
2022-11-20 11:53:04 -0300

    exercicio finalizado

---
+++ Course/src/application/Program.java
@@ -0,0 +1,52 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		System.out.println("Enter the number of products");
+		int n = sc.nextInt();
+
+		List<Product> list = new ArrayList<>();
+		
+		for(int i = 1; i<= n; i++) {
+			System.out.println("Product #" + i + "data: ");
+			System.out.print("Comon, used or imported (u, i, c");
+			char type = sc.next().charAt(0);
+			System.out.print("Name: ");
+			String name = sc.next();
+			double price = sc.nextDouble();
+			if (type == 'c' ) {
+				list.add(new Product(name,price));
+			}else if (type == 'i') {
+				System.out.print("Customs fee");
+				double customsFee = sc.nextDouble();
+				list.add(new ImportedProduct(name, price, customsFee));
+			}else {
+				System.out.print("Manufacture date (DD/MM/YYYY): ");
+				Date date = sdf.parse(sc.next());
+				list.add(new UsedProduct(name, price, date));
+				
+				
+			}
+		}
+
+		sc.close();
+
+	}
+
+}
